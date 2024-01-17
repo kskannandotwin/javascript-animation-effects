4fb406e3198f69e5779caca628ea3860e56899fd
kskannandotwin <kskannan.win@gmail.com>
2024-01-17 22:51:26 +0530

    17.01.2024-wab
    completed

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let pattern = document.querySelector(".bg");
+let section = document.querySelector("section");
+let j = 0;
 
-window.addEventListener("scroll", () => {
-  pattern.style.backgroundPosition = window.scrollY + "px";
-});
+while (j < 200) {
+  let star = document.createElement("i");
+  let x = Math.floor(Math.random() * window.innerWidth);
+  star.style.left = `${x}px`;
+  let y = Math.floor(Math.random() * window.innerHeight);
+  star.style.top = `${y}px`;
+
+  let size = Math.random() * 4;
+  star.style.width = `${1 + size}px`;
+  star.style.height = `${1 + size}px`;
+
+  let duration = Math.random() * 2;
+  star.style.animationDuration = `${duration + 1}s`;
+
+  section.appendChild(star);
+  j++;
+}
