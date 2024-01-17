da741892e53e2baf4986f61b5c0f2b590a1eb9c0
kskannandotwin <kskannan.win@gmail.com>
2024-01-17 19:08:46 +0530

    17.01.2024-wpe
    completed

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-let text = document.querySelector(".text");
-let spans = text.innerText
-  .split("")
-  .map((letter, i) => {
-    return `<span style="transition-delay:${i * 40}ms; filter:hue-rotate(${
-      i * 30
-    }deg)">${letter}</span>`;
-  })
-  .join("");
+let text = document.getElementById("text");
+let leaf = document.getElementById("leaf");
+let hill1 = document.getElementById("hill1");
+let hill4 = document.getElementById("hill4");
+let hill5 = document.getElementById("hill5");
 
-text.innerHTML = spans;
-console.log(spans);
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+
+  text.style.marginTop = `${value * 2.5}px`;
+  leaf.style.top = `${value * -1.5}px`;
+  leaf.style.left = `${value * 1.5}px`;
+  hill5.style.left = `${value * 1.5}px`;
+  hill4.style.left = `${value * -1.5}px`;
+  hill1.style.top = `${value * 1}px`;
+});
