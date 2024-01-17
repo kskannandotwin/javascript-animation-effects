c4dc95084bfd74387c8065544b96601b7cbaf5c7
kskannandotwin <kskannan.win@gmail.com>
2024-01-17 22:56:39 +0530

    17.01.2024-nmra
    completed

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-let section = document.querySelector("section");
-let j = 0;
-
-while (j < 200) {
-  let star = document.createElement("i");
-  let x = Math.floor(Math.random() * window.innerWidth);
-  star.style.left = `${x}px`;
-  let y = Math.floor(Math.random() * window.innerHeight);
-  star.style.top = `${y}px`;
-
-  let size = Math.random() * 4;
-  star.style.width = `${1 + size}px`;
-  star.style.height = `${1 + size}px`;
-
-  let duration = Math.random() * 2;
-  star.style.animationDuration = `${duration + 1}s`;
-
-  section.appendChild(star);
-  j++;
-}
+const sun = document.querySelector(".sun");
+const scene = document.querySelector(".scene");
+sun.addEventListener("click", () => {
+  scene.classList.toggle("night");
+});
