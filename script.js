5360730ae37d5fa9cd5d950c29809896ad18ac3d
kskannandotwin <kskannan.win@gmail.com>
2024-01-17 22:21:05 +0530

    17.01.2024-cpae
    completed

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-let text = document.querySelector(".text");
+let circle1 = document.querySelector(".circle1");
+let circle2 = document.querySelector(".circle2");
 
-let htmlSpans = text.innerText
-  .split("")
-  .map((letter, i) => {
-    return `<span style= "animation-duration: ${
-      Math.random() * 5
-    }s; filter: hue-rotate(${i * 50}deg">${letter}</span>`;
-  })
-  .join("");
-
-text.innerHTML = htmlSpans;
-
-console.log(htmlSpans);
+window.addEventListener("scroll", () => {
+  let scrollValue = window.scrollY;
+  circle1.style.clipPath = `circle(${150 + scrollValue * 0.75}px at 0 0)`;
+  circle2.style.clipPath = `circle(${150 + scrollValue * 0.75}px at 100% 100%)`;
+});
