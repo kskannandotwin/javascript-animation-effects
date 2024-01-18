32d9666692cb50b7f9facb1bde0538bdc2ff1430
kskannandotwin <kskannan.win@gmail.com>
2024-01-18 05:45:08 +0530

    18.01.2024-tna
    completed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let container = document.querySelector(".container");
 let toggle = document.querySelector(".toggle");
 
 toggle.addEventListener("click", () => {
-  container.classList.toggle("active");
+  toggle.classList.toggle("active");
 });
